918268c952d9e3f9812073e1b6d5c0c22e102d0b
Shantanu-thapa <shantanuthapa021@gmail.com>
2026-09-26 17:21:24 +0530

    Remove default API URL fallback

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -17,7 +17,7 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./App.css";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+  import.meta.env.VITE_API_URL ;
 
 function App() {
   const [projects, setProjects] = useState([]);
