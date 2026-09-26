c630063dcdb91817fe681706629b61868594176a
Shantanu-thapa <shantanuthapa021@gmail.com>
2026-09-26 16:23:10 +0530

    Update resume download function to use axios
    Refactor handleResumeDownload to use axios for fetching resume URL before downloading.

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -195,10 +195,49 @@ function App() {
   // DOWNLOAD RESUME
 
   // =========================
+const handleResumeDownload = async () => {
 
-const handleResumeDownload = () => {
-  window.open(`${API_URL}/resume/download`, "_blank");
-};
+    try {
+
+      const response = await axios.get(
+
+        ${API_URL}/resume/download
+
+      );
+
+
+
+      const resumeURL = response.data.resumeURL;
+
+
+
+      if (!resumeURL) {
+
+        console.error("Resume URL not found");
+
+        return;
+
+      }
+
+
+
+      window.open(resumeURL, "_blank");
+
+    } catch (error) {
+
+      console.error(
+
+        "Resume download error:",
+
+        error.response?.data || error.message
+
+      );
+
+    }
+
+  };
+
+
 
 
   // =========================
